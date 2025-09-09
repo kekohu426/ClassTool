b6ee427fcbf18fec0eb903e84f31577e59a1b3be
kekohu426 <hukeko0206@gmail.com>
2025-09-09 16:52:10 +0800

    Delete Downloads/aihuabu directory

--- Downloads/aihuabu/src/components/ui/aspect-ratio.tsx
+++
@@ -1,5 +0,0 @@
-import * as AspectRatioPrimitive from "@radix-ui/react-aspect-ratio@1.1.0";
-
-const AspectRatio = AspectRatioPrimitive.Root;
-
-export { AspectRatio };
--- Downloads/aihuabu/src/components/ui/collapsible.tsx
+++
@@ -1,9 +0,0 @@
-import * as CollapsiblePrimitive from "@radix-ui/react-collapsible@1.1.1";
-
-const Collapsible = CollapsiblePrimitive.Root;
-
-const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger;
-
-const CollapsibleContent = CollapsiblePrimitive.CollapsibleContent;
-
-export { Collapsible, CollapsibleTrigger, CollapsibleContent };
